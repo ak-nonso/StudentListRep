739ecedf3284a7e5d7578a4a9624bcde52ba4dc2
ak-nonso <akudigwe@sheridancollege.ca>
2026-05-11 10:31:47 -0400

    student project master

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author akudi
+ */
+public class StudentList {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1 = new Student(); //s1 is an object
+        s1.setName("Peter");
+        s1.setSid(1);
+        Student s2 = new Student();
+        s2.setName("John");
+        s2.setSid(2);
+        Student[] list = new Student[2]; //object of student class
+        list[0] = s1;
+        list[1] = s2;
+        for (int i = 0; i < list.length; i++){
+            System.out.println(list[i].getName() + " " + list[i].getSid());
+        }
+    }
+    
+}
